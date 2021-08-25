ba0aae076d2fa7976a99c373a91e2b7257d45762
seulgi1214 <leeseulgi1214@gmail.com>
2021-08-25 20:46:24 +0900

    api key 변경

--- src/views/RealTime.vue
+++ src/views/RealTime.vue
@@ -24,7 +24,7 @@ export default {
   data(){
     return {
       base: 'https://api.nytimes.com/svc/news/v3/content/all/all.json',
-      api_key: 'process.env.VUE_APP_api_key',
+      api_key: process.env.VUE_APP_api_key,
       news: [{
 
       }]
